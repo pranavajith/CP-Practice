307c536f3e60e5ae99a4c2551bcd8f5535ea7cd2
pranavajith <96235918+pranavajith@users.noreply.github.com>
2023-09-11 22:19:00 +0530

    New Problem

---
+++ A new Journey/A_Game_with_Board.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <set>
+#include <cmath>
+#include <map>
+#include <sstream>
+#include <deque>
+#include <algorithm>
+#include <limits>
+#include <iomanip>
+using namespace std;
+#define ll long long
+// #define TxtIO   freopen("input.txt","r",stdin); freopen("output.txt","w",stdout); freopen("error.txt", "w", stderr);
+
+void solve(){
+    ll a;
+    cin>>a;
+    if (a%2)cout<<"Bob\n";
+    else cout<<"Alice\n";
+}
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // TxtIO;
+    ll t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
