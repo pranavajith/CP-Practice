1f2bbf4295cfb73257f082d5feed9285a2595851
pranavajith <96235918+pranavajith@users.noreply.github.com>
2023-09-20 16:53:46 +0530

    Average Day

--- C_Make_It_Permutation.cpp
+++ C_Make_It_Permutation.cpp
@@ -11,56 +11,28 @@
 using namespace std;
 #define ll long long
 // #define TxtIO   freopen("input.txt","r",stdin); freopen("output.txt","w",stdout); freopen("error.txt", "w", stderr);
-
-void solve(){
-    long long int n,c,d, duplisum = 0;
-    cin>>n>>d>>c;
-    vector <long long int> v;
-    map <long long int,long long int> m;
-    for (int i=0;i<n;i++){
-        long long int a;
-        cin>>a;
-        if (m[a])duplisum+=d;
-        else{
-            m[a]++;
-            v.push_back(a);
-        }
-    }
-    sort(v.begin(),v.end());
-    long long int i = 0;
-    n=v.size();
-    while (i!=n && i+1==v[i])i++;
-    if (i==n)cout<<duplisum<<"\n"; 
-    else{
-        // i--;
-        long long int fin=0;
-        if (i!=0)fin = (n-i)*d;
-        else fin = n*d + c;
-        long long int pointer = i;
-        long long int temp = fin;
-        while (pointer!=n){
-            // long long int temp;
-            // if (pointer == 0){
-            //     temp = d*n;
-            // }
-            // else{
-                temp += (v[pointer]-v[pointer-1]-1)*c - d;
-            // }
-            if (fin <= temp)break;
-            fin = temp;
-            pointer++;
-        }
-        cout<<fin+duplisum<<"\n";
-        // cout<<duplisum<<"!!!\n";
-    }
-}
-
-int main() 
+int p[100005];
+// typedef long long ll;
+void solve()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    // TxtIO;
-    long long int t; cin>>t; while(t--)
-        solve();
-    return 0;
+    int n,a,b;scanf("%d%d%d",&n,&a,&b);
+    set<int> st;
+    ll sol = 0 , ans = 2e18;
+    for(int i = 1;i <= n;i++) {
+        int x;scanf("%d",&x);
+        if(st.find(x) == st.end()) st.insert(x);
+        else sol += a;
+    }
+    int c = 0;
+    for(auto x : st) p[++c] = x;
+    for(int i = 1;i <= c;i++) {
+        ans = min(ans , 1LL*(p[i] - i)*b + 1LL*(c-i)*a);
+    }
+    ans = min(ans , 1LL*c*a + b) ;
+    printf("%lld\n",ans+sol);
+}
+int main()
+{
+    int t;scanf("%d",&t);
+    while(t--) solve();
 }
