4750f573159f5ad84880586652d77754dc5decc7
pranavajith <96235918+pranavajith@users.noreply.github.com>
2023-09-21 10:46:32 +0530

    Was able to solve Cake Assembly

--- A_Ezzat_and_Two_Subsequences.cpp
+++ A_Ezzat_and_Two_Subsequences.cpp
@@ -12,21 +12,31 @@ using namespace std;
 #define ll long long
 // #define TxtIO   freopen("input.txt","r",stdin); freopen("output.txt","w",stdout); freopen("error.txt", "w", stderr);
 
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        long long max1 = INT8_MIN;
-        double sum = 0;
-        for(int i = 0 ; i < n ;i++){
-            long long values;
-            cin >> values;
-            sum = sum + values;
-            max1 = max(max1, values);
-        }
-        double result = (sum - max1)/(n - 1) + max1;
-        cout << fixed << result << endl;
+void solve(){
+    double n;
+    double sum1=0;
+    cin>>n;
+    vector <ll> v;
+    for (int i=0;i<n;i++){
+        double a;
+        cin>>a;
+        v.push_back(a);
     }
+    sort(v.begin(),v.end());
+    for (int i=0;i<n-1;i++){
+        sum1+=v[i];
+    }
+    // double ans = sum1
+    cout<<fixed<<sum1/(n-1)+v[n-1]<<"\n";
+
+}
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // TxtIO;
+    ll t; cin>>t; while(t--)
+        solve();
+    return 0;
 }
