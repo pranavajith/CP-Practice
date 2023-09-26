e4a5967fd66fdf83d1d80ebdf1d96157958f2ecb
pranavajith <96235918+pranavajith@users.noreply.github.com>
2023-09-26 10:16:46 +0530

    done

--- B_Hamon_Odyssey.cpp
+++ B_Hamon_Odyssey.cpp
@@ -13,22 +13,7 @@ using namespace std;
 // #define TxtIO   freopen("input.txt","r",stdin); freopen("output.txt","w",stdout); freopen("error.txt", "w", stderr);
 
 void solve(){
-    ll n;
-    cin>>n;
-    bool check = false;
-    vector <ll> v;
-    if (n%2==0)cout<<-1<<"\n";
-    else {
-        for (int i=31; i>0; i--){
-            if ((n>>i) & 1){
-                v.push_back(2);
-                check = true;
-            }
-            else if (check == true)v.push_back(1);
-        }
-        cout<<v.size()<<"\n";
-        for (auto d:v)cout<<d<<" ";cout<<"\n";
-    }
+
 }
 
 int main() 
