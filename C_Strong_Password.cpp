2dcd7451e6ca783b9328ce46ebf7535079090b95
pranavajith <96235918+pranavajith@users.noreply.github.com>
2023-09-29 16:27:04 +0530

    Been a while

---
+++ C_Strong_Password.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <set>
+#include <cmath>
+#include <map>
+#include <sstream>
+#include <deque>
+#include <algorithm>
+#include <limits>
+#include <iomanip>
+using namespace std;
+#define ll long long
+// #define TxtIO   freopen("input.txt","r",stdin); freopen("output.txt","w",stdout); freopen("error.txt", "w", stderr);
+
+void solve(){
+    string s;
+    cin>>s;
+    ll n;
+    cin>>n;
+    string s1, s2;
+    cin>>s1>>s2;
+    ll iter = 0;
+    while (iter!=s.size()){
+        
+    }
+}
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // TxtIO;
+    ll t; cin>>t; while(t--)
+        solve();
+    return 0;
+}
